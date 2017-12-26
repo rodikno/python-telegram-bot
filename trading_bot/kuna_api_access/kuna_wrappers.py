62f43502f3886c3d8c8ed73c749c399193511a99
Rodion Baronov <rodikno@gmail.com>
2017-12-26 19:21:42 +0200

    Tested wrapper and it works, moving to bot itself

--- trading_bot/kuna_api_access/kuna_wrappers.py
+++ trading_bot/kuna_api_access/kuna_wrappers.py
@@ -1,6 +1,10 @@
-import kuna.kuna
+import kuna.kuna as kuna
 import trading_bot.kuna_api_access.kuna_local_config as kuna_config
 
-graph_kuna = kuna.kuna.KunaAPI(access_key=kuna_config.KUNA_API_PUBLIC_KEY,
+graph_kuna = kuna.KunaAPI(access_key=kuna_config.KUNA_API_PUBLIC_KEY,
                           secret_key=kuna_config.KUNA_SECRET_KEY)
 
+pairs = kuna.VALID_MARKET_DATA_PAIRS
+market_data = graph_kuna.get_recent_market_data('btcuah')
+
+print(market_data['ticker'])
--- trading_bot/main.py
+++ trading_bot/main.py
@@ -1,6 +1,9 @@
+import logging
+
 from telegram.ext import Updater
 from telegram.ext import CommandHandler
-import logging
+import kuna.kuna as kuna
+import trading_bot.kuna_api_access.kuna_local_config as kuna_config
 
 import trading_bot.bot_local_config as cfg
 
@@ -9,8 +12,12 @@ dispatcher = updater.dispatcher
 
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
 
+#Setup KunaAPI connection
+graph_kuna = kuna.KunaAPI(access_key=kuna_config.KUNA_API_PUBLIC_KEY,
+                          secret_key=kuna_config.KUNA_SECRET_KEY)
+
 def start(bot, update):
-    bot.send_message(chat_id=update.message.chat_id, text="I'm a bot, please talk to me!")
+    bot.send_message(chat_id=update.message.chat_id, text="Привет, я бот который поможет тебе торговать на бирже KUNA.IO")
 
 def caps(bot, update, args):
     text_caps = ' '.join(args).upper()
