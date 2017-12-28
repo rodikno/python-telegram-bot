5a2c9c0226300d7fe92ee9c08ac54a8e43eb5eb6
Rodion Baronov <rodikno@gmail.com>
2017-12-29 00:17:29 +0200

    Added exchange pairs buttons

--- trading_bot/kuna_api_access/kuna_wrappers.py
+++ trading_bot/kuna_api_access/kuna_wrappers.py
@@ -1,10 +1,12 @@
 import kuna.kuna as kuna
 import trading_bot.kuna_api_access.kuna_local_config as kuna_config
 
-graph_kuna = kuna.KunaAPI(access_key=kuna_config.KUNA_API_PUBLIC_KEY,
+kuna_api = kuna.KunaAPI(access_key=kuna_config.KUNA_API_PUBLIC_KEY,
                           secret_key=kuna_config.KUNA_SECRET_KEY)
 
 pairs = kuna.VALID_MARKET_DATA_PAIRS
-market_data = graph_kuna.get_recent_market_data('btcuah')
+market_data = kuna_api.get_recent_market_data('btcuah')
+
+
 
 print(market_data['ticker'])
--- trading_bot/native_api_bot.py
+++ trading_bot/native_api_bot.py
@@ -1,8 +1,18 @@
 import telebot
 from telebot import types
 import trading_bot.bot_local_config as cfg
+import kuna.kuna as kuna
+
+import trading_bot.kuna_api_access.kuna_local_config as kuna_config
 
 bot = telebot.TeleBot(cfg.BOT_TOKEN)
+
+kuna_api = kuna.KunaAPI(access_key=kuna_config.KUNA_API_PUBLIC_KEY,
+                        secret_key=kuna_config.KUNA_SECRET_KEY)
+
+
+# market_data = kuna_api.get_recent_market_data('btcuah')
+
 
 
 @bot.message_handler(commands=['start', 'help'])
@@ -12,14 +22,25 @@ def send_welcome(message):
     btn_latest_market_data = types.InlineKeyboardButton(text='Данные по рынку', callback_data='market_data')
 
     markup.add(btn_exchange_rates, btn_latest_market_data)
-    bot.send_message(message.chat.id, 'Please choose one of the options:', reply_markup=markup)
+    bot.send_message(message.chat.id, text='Please choose one of the options:', reply_markup=markup)
 
 
 @bot.callback_query_handler(func=lambda call: True)
 def callback_inline(call):
     if call.message:
         if call.data == 'exchange_rates':
-            bot.send_message(chat_id=call.message.chat.id, text='Called Exchange rates mthfck!')
+            markup = types.ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True)
+
+            btn_btcuah  = types.InlineKeyboardButton(text='BTC/UAH', callback_data='exchange_rate_btcuah')
+            btn_ethuah = types.InlineKeyboardButton(text='ETH/UAH', callback_data='exchange_rate_ethuah')
+            btn_wavesuah = types.InlineKeyboardButton(text='WAVES/UAH', callback_data='exchange_rate_wavesuah')
+            btn_gbguah = types.InlineKeyboardButton(text='GBG/UAH', callback_data='exchange_rate_gbguah')
+            btn_kunbtc = types.InlineKeyboardButton(text='KUN/BTC', callback_data='exchange_rate_kunbtc')
+            btn_bchbtc = types.InlineKeyboardButton(text='BCH/BTC', callback_data='exchange_rate_bchbtc')
+
+            markup.add(btn_btcuah, btn_ethuah, btn_wavesuah, btn_gbguah, btn_kunbtc, btn_bchbtc)
+
+            bot.send_message(chat_id=call.message.chat.id, text='Please choose a currency pair:', reply_markup=markup)
         if call.data == 'market_data':
             bot.send_message(chat_id=call.message.chat.id, text='Called market data mthfucka')
     # elif call.inline_message_id:
